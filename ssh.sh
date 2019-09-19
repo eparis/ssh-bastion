bd7d407dbcee6e98ba77e2d6977af056b2329868
Petr Balogh <pbalogh@redhat.com>
2019-09-19 15:50:04 +0200

    Allow to use different SSH key for connection
    Signed-off-by: Petr Balogh <pbalogh@redhat.com>

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ssh -t -o StrictHostKeyChecking=no -o ProxyCommand='ssh -A -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -W %h:%p core@$(oc get service --all-namespaces -l run=ssh-bastion -o jsonpath="{.items[0].status.loadBalancer.ingress[0].hostname}")' core@$1 "sudo -i"
+ssh_key_param=''
+if [ ! -z "$SSH_KEY_PATH" ]; then
+    ssh_key_param="-i $SSH_KEY_PATH"
+fi
+
+ssh $ssh_key_param -t -o StrictHostKeyChecking=no -o ProxyCommand="ssh $ssh_key_param -A -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -W %h:%p core@$(oc get service --all-namespaces -l run=ssh-bastion -o jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')" core@$1 "sudo -i"
